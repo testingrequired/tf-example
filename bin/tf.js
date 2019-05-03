69108840ce6ffbc1373b709d30fd4d10ed65d1a9
Kylee Tilley <kyleetilley@gmail.com>
2019-05-02 21:31:35 -0500

    Update tf version
    - Also moved tf.js to bin

--- bin/tf.js
+++ bin/tf.js
@@ -3,5 +3,10 @@ import tf, { run, middlewear } from "@testingrequired/tf";
 const { defaults, matchTestFiles, specSyntax, junit } = middlewear;
 
 run(
-  tf(defaults, matchTestFiles("./**/*.test.js"), specSyntax, junit("junit.xml"))
+  tf(
+    defaults,
+    matchTestFiles("./tests/**/*.test.js"),
+    specSyntax,
+    junit("junit.xml")
+  )
 );
